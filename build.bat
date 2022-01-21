58f6f90bd0708d95da884d8dc0715d0011922c64
bearki <bearki@126.com>
2022-01-21 11:37:30 +0800

    update v0.0.5

--- build.bat
+++ build.bat
@@ -2,10 +2,10 @@ chcp 65001
 
 SET GOOS=windows
 SET GOARCH=386
-go build -ldflags "-s -w" -o gov0.0.4.%GOOS%-%GOARCH%.exe .
-upx gov0.0.4.%GOOS%-%GOARCH%.exe
+go build -ldflags "-s -w" -o .\build\gov0.0.5.%GOOS%-%GOARCH%.exe .
+upx -9 .\build\gov0.0.5.%GOOS%-%GOARCH%.exe
 
 SET GOOS=windows
-SET GOARCH=adm64
-go build -ldflags "-s -w" -o gov0.0.4.%GOOS%-%GOARCH%.exe .
-upx gov0.0.4.%GOOS%-%GOARCH%.exe
+SET GOARCH=amd64
+go build -ldflags "-s -w" -o .\build\gov0.0.5.%GOOS%-%GOARCH%.exe .
+upx -9 .\build\gov0.0.5.%GOOS%-%GOARCH%.exe
