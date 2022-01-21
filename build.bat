86df96e8773ee94e9e35d25b06ba232a637efcae
bearki <bearki@126.com>
2022-01-21 12:19:28 +0800

    update

--- build.bat
+++ build.bat
@@ -1,11 +1,15 @@
 chcp 65001
 
+SET VERSION=0.0.6
+
 SET GOOS=windows
 SET GOARCH=386
-go build -ldflags "-s -w" -o .\build\gov0.0.5.%GOOS%-%GOARCH%.exe .
-upx -9 .\build\gov0.0.5.%GOOS%-%GOARCH%.exe
+SET OUTFILE=.\build\gov%VERSION%.%GOOS%-%GOARCH%.exe
+go build -ldflags "-s -w" -o %OUTFILE% .
+upx -9 %OUTFILE%
 
 SET GOOS=windows
 SET GOARCH=amd64
-go build -ldflags "-s -w" -o .\build\gov0.0.5.%GOOS%-%GOARCH%.exe .
-upx -9 .\build\gov0.0.5.%GOOS%-%GOARCH%.exe
+SET OUTFILE=.\build\gov%VERSION%.%GOOS%-%GOARCH%.exe
+go build -ldflags "-s -w" -o %OUTFILE% .
+upx -9 %OUTFILE%
