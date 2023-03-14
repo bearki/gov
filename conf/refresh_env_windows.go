843a4a9f27f5725d219999c131d56b21cddd845d
bearki <bearki@126.com>
2023-03-14 17:48:55 +0800

    移除无效代码

--- conf/refresh_env_windows.go
+++ conf/refresh_env_windows.go
@@ -3,11 +3,43 @@
 package conf
 
 import (
+	"fmt"
 	"syscall"
 	"unsafe"
 
 	"github.com/bearki/gov/tool"
+	"golang.org/x/sys/windows/registry"
 )
+
+// WriteEnv 写入到环境变量
+//
+// @params name 环境变量值
+//
+// @params value 环境变量值
+//
+// @params valType 环境变量的值类型（1-字符串值，2-可扩充字符串值）
+//
+// @return 异常信息
+func WriteEnv(name, value string, valType int) error {
+	// 创建项目键值，若已存在，则直接返回键值，所有权限
+	projectKey, _, err := registry.CreateKey(
+		registry.CURRENT_USER,
+		"Environment",
+		registry.ALL_ACCESS,
+	)
+	if err != nil {
+		return err
+	}
+	defer projectKey.Close()
+	// 写入key-val到注册表
+	if valType == 1 {
+		return projectKey.SetStringValue(name, value)
+	}
+	if valType == 2 {
+		return projectKey.SetExpandStringValue(name, value)
+	}
+	return fmt.Errorf("not support")
+}
 
 // refreshEnv 刷新操作系统环境变量
 func refreshEnv() {
