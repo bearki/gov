4328f57253cde3d428f8968cf724850102df3321
bearki <bearki@126.com>
2023-03-14 15:50:50 +0800

    优化windows环境下的环境变量写入

--- tool/registry_table_windows.go
+++ tool/registry_table_windows.go
@@ -13,12 +13,16 @@ import (
 	"golang.org/x/sys/windows/registry"
 )
 
-// SetRegistryValue 设置注册表键值
-// @params name    string 键名
-// @params value   string 键值
-// @params valType int    值类型（1-字符串值，2-可扩充字符串值）
-// @return         error  错误信息
-func SetRegistryValue(name string, value string, valType int) error {
+// WriteEnv 写入到环境变量
+//
+// @params name 环境变量值
+//
+// @params value 环境变量值
+//
+// @params valType 环境变量的值类型（1-字符串值，2-可扩充字符串值）
+//
+// @return 异常信息
+func WriteEnv(name, value string, valType int) error {
 	// 创建项目键值，若已存在，则直接返回键值，所有权限
 	projectKey, _, err := registry.CreateKey(
 		registry.CURRENT_USER,
@@ -37,34 +41,4 @@ func SetRegistryValue(name string, value string, valType int) error {
 		return projectKey.SetExpandStringValue(name, value)
 	}
 	return fmt.Errorf("not support")
-}
-
-// GetRegistryValue 获取注册表键值
-// @params name  string 键名
-// @return       string 键值
-// @return       error  错误信息
-func GetRegistryValue(name string) (string, error) {
-	// 创建项目键值，若已存在，则直接返回键值，所有权限
-	projectKey, _, err := registry.CreateKey(
-		registry.CURRENT_USER,
-		"Environment",
-		registry.ALL_ACCESS,
-	)
-	if err != nil {
-		return "", err
-	}
-	// 延迟关闭
-	defer projectKey.Close()
-	// 从注册表获取key对应的值
-	val, _, err := projectKey.GetStringValue(name)
-	// 判断是否发生错误
-	if err != nil {
-		// 不存在时反空值
-		if err == registry.ErrNotExist {
-			return "", nil
-		}
-		return "", err
-	}
-	// 获取到设备SN
-	return val, nil
 }
